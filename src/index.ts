e010277fa69d6fa41469418ca06a180c8711de94
Maratib Ali Khan <maratib@gmail.com>
2024-12-01 01:21:10 +0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import clear from 'clear';
+import chalk from 'chalk';
+import figlet from 'figlet';
+// import path from 'path';
+// import program from 'commander';
 
 
-console.log("Create some cli");
+clear();
+
+console.log(
+    chalk.red(
+        figlet.textSync('pizza-cli', { horizontalLayout: 'full' })
+    )
+);
